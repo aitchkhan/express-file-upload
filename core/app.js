96320f570bfaa4e5b70986a5dddd93316015aa11
Haroon <aitchkhan@gmail.com>
2018-04-08 09:23:17 +0500

    file upload using multer example from workshop

--- core/app.js
+++ core/app.js
@@ -1,32 +1,27 @@
 const express = require('express')
 const path = require('path')
 const timeout = require('connect-timeout')
-// const favicon = require('serve-favicon')
 const logger = require('morgan')
-const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const favicon = require('serve-favicon')
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const files = require('./routes/files')
 
 const app = express()
 
-// uncomment after placing your favicon in /public
 app.use(timeout('2m')) // timeout after 2mins
 app.use(logger('dev'))
 app.use(bodyParser.json({limit: '5mb'}))
 app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }))
-app.use(haltOnTimedout)
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(haltOnTimeout)
+app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')))
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
-app.use('/', index)
-app.use('/users', users)
+app.use('/files', files)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -41,15 +36,15 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-
   res.status(err.status || 500)
   res.json({
     err: 'error',
-    message: err.message || 'internal server error'
-})
+    message: err.message || 'internal server error',
+    stack: err.stack || 'internal server error'
+  })
 })
 
-function haltOnTimedout (req, res, next) {
+function haltOnTimeout (req, res, next) {
   if (!req.timedout) {
     next()
   }
